d4deab0bb2587799e9ad020cc9a173cc4a7b9892
Richard MEnne <menneric84@gmail.com>
2018-08-23 12:22:37 -0500

    added more cases

--- src/test/java/Stepdefs.java
+++ src/test/java/Stepdefs.java
@@ -1,19 +1,18 @@
-import cucumber.api.PendingException;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.winium.DesktopOptions;
 import org.openqa.selenium.winium.WiniumDriver;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
 
 
@@ -27,31 +26,56 @@ public class Stepdefs {
     private String result;
     private DesktopOptions options;
 
+    @Before()
+    public void beforeScenarioStart() throws MalformedURLException {
+        options = new DesktopOptions();
+        options.setApplicationPath(CALC_PATH);
+        driver = new WiniumDriver(new URL(WINIUM_PATH), options);
+    }
+
     @Given("^I want to add \"([^\"]*)\" to \"([^\"]*)\"$")
-    public void iWantToAddTo(int number1, int number2) throws MalformedURLException {
+    public void iWantToAddTo(int number1, int number2) {
+        System.out.println(number1);
+        System.out.println(number2);
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    @When("I add")
+    public void add() {
+        driver.findElement(By.id("num" + number1 + "Button")).click();
+        driver.findElement(By.id("plusButton")).click();
+        driver.findElement(By.id("num" + number2 + "Button")).click();
+        driver.findElement(By.id("equalButton")).click();
+        result = driver.findElement(By.id("CalculatorResults")).getAttribute("Name");
+    }
+
+    @Then("^my answer should equal \"([^\"]*)\"$")
+    public void myAnswerShouldEqual(String result) {
+        assertEquals(result, this.result);
+    }
+
+    @After
+    public void cleanUp() throws InterruptedException {
+        driver.findElement(By.id("Close")).click();
+    }
+
+    @Given("^I want to subtract \"([^\"]*)\" from \"([^\"]*)\"$")
+    public void iWantToSubtractFrom(int number1, int number2) throws Throwable {
         options = new DesktopOptions();
         options.setApplicationPath(CALC_PATH);
         driver = new WiniumDriver(new URL(WINIUM_PATH), options);
         this.number1 = number1;
         this.number2 = number2;
     }
-    @When("I add")
-    public void add(){
-        driver.findElement(By.id("num7Button")).click();
-        driver.findElement(By.id("plusButton")).click();
-        driver.findElement(By.id("num3Button")).click();
+
+    @When("^I subtract$")
+    public void iSubtract() throws Throwable {
+
+        driver.findElement(By.id("num" + number1 + "Button")).click();
+        driver.findElement(By.id("minusButton")).click();
+        driver.findElement(By.id("num" + number2 + "Button")).click();
         driver.findElement(By.id("equalButton")).click();
         result = driver.findElement(By.id("CalculatorResults")).getAttribute("Name");
     }
-
-    @Then("^my answer should equal \"([^\"]*)\"$")
-    public void myAnswerShouldEqual(String result){
-        assertEquals(result, this.result);
-    }
-
-    @After
-    public void cleanUp(){
-        driver.findElement(By.id("Close")).click();
-    }
-
 }
--- src/test/resources/calculator.feature
+++ src/test/resources/calculator.feature
@@ -1,5 +1,7 @@
 Feature: Calculator
 
+  Background: Calculator is up
+
   Scenario Outline: Add a few numbers
     Given I want to add "<numberOne>" to "<numberTwo>"
     When I add
@@ -8,4 +10,18 @@ Feature: Calculator
     Examples:
       | numberOne | numberTwo | result        |
       | 7         | 3         | Display is 10 |
-      | 14        | 6         | Display is 20 |
+      | 4         | 6         | Display is 10 |
+      | 1         | 1         | Display is 2  |
+      | 3         | 2         | Display is 5  |
+
+  Scenario Outline: Subtract a few numbers
+    Given I want to subtract "<numberOne>" from "<numberTwo>"
+    When I subtract
+    Then my answer should equal "<result>"
+
+    Examples:
+      | numberOne | numberTwo | result        |
+      | 7         | 3         | Display is 4  |
+      | 4         | 6         | Display is -2 |
+      | 8         | 2         | Display is 6  |
+
